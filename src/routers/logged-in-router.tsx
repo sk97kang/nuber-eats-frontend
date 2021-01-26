27c7b2da03c8ec6783865755dcd137e3b37870d5
sk97kang <sk97.kang@gmail.com>
2021-01-26 20:43:09 +0900

    #15 AUTHENTICATION

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,3 +1,38 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { Header } from "../components/header";
+import { useMe } from "../hooks/useMe";
+import { Restaurants } from "../pages/client/restaurants";
+import { UserRole } from "../__generated__/globalTypes";
 
-export const LoggedInRouter = () => <span>Logged In</span>;
+const ClientRoutes = [
+  <Route path="/" exact key="restaurants">
+    <Restaurants />
+  </Route>,
+];
+
+export const LoggedInRouter = () => {
+  const { data, loading, error } = useMe();
+
+  if (!data || loading || error) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <span className="font-medium text-xl tracking-wide">Loading...</span>
+      </div>
+    );
+  }
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        {data.me.role === UserRole.Client && ClientRoutes}
+        <Redirect from="*" to="/" />
+      </Switch>
+    </Router>
+  );
+};
